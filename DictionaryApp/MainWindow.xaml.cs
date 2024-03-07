5d0ba4f8b75fc17adf586a4e7f2d82d94134c7ca
anndra17 <andra.horhocea@student.unitbv.ro>
2024-03-07 20:17:41 +0200

    Implementation of the GUI and the functionality of the WordSearch class.
    The simplified version of what will be WordSearch.

--- DictionaryApp/MainWindow.xaml.cs
+++ DictionaryApp/MainWindow.xaml.cs
@@ -24,5 +24,40 @@ namespace DictionaryApp
         {
             InitializeComponent();
         }
+
+        private void InputButton_Click(object sender, EventArgs e)
+        {
+            var My_Dictionary = new Dictionary<string, string>()
+            {
+                {"masă", "O piesă de mobilier pentru luat masa"},
+                {"soare", "Sursă principală de lumină și căldură pentru Pământ"},
+                {"câine", "Un animal domestic din familia canidelor"},
+                {"carte", "Un obiect cu pagini scrise, legate împreună"},
+                {"munte", "O înălțime naturală, cu vârf ascuțit"},
+                {"computer", "Un dispozitiv electronic pentru prelucrarea informațiilor"},
+                {"floare", "O parte a unei plante, adesea colorată și parfumată"},
+                {"școală", "O instituție de învățământ"},
+                {"râu", "Un curs de apă natural, mai mic decât un fluviu"},
+                {"pian", "Un instrument muzical cu clape"},
+                {"paraxin", "Bizar, ciudat, curios." }
+            };
+
+            string word;
+            word = InputBox.Text;
+
+            try
+            {
+                ResultBox.Text = My_Dictionary[word];
+            }
+            catch
+            {
+                ResultBox.Text = "Scuze! Cuvântul nu a fost găsit!";
+            }
+        }
+
+        private void InputButton_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
