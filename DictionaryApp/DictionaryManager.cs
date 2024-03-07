97074cad69f626308276a2c4acbc302054118145
anndra17 <andra.horhocea@student.unitbv.ro>
2024-03-07 20:15:10 +0200

    Creation of DictionaryManager, WordSearch and Entertainment classes
    These classes represent the basis on which the application will be built.

---
+++ DictionaryApp/DictionaryManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryApp
+{
+    internal class DictionaryManager
+    {
+        public DictionaryManager() { }
+        public void AddWord(string word, string definition, string category, string image)
+        {
+
+        }
+
+        public void RemoveWord(string word)
+        {
+
+        }
+
+        public void ModifyWord(string word) { }
+    }
+}
