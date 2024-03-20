7e28379754a5c2f3b48c7aabb65216e0702bf584
anndra17 <andra.horhocea@student.unitbv.ro>
2024-03-20 14:26:51 +0200

    Changes related to the GUI
    -Adding icons for each module
    -Add page title images

--- DictionaryApp/DictionaryManager.xaml.cs
+++ DictionaryApp/DictionaryManager.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +21,7 @@ namespace DictionaryApp
         public string Username { get; set; }
         public string Password { get; set; }
         public string PlaceholderText { get; set; }
+        public string imagePath {  get; set; }
         public DictionaryManager()
         {
             InitializeComponent();
@@ -27,15 +29,13 @@ namespace DictionaryApp
             
         }
 
-
-
         private void LoginButton_Click(object sender, RoutedEventArgs e)
         {
             Username = UsernameBox.Text;
             Password = PasswordBox.Password;
 
             List<Account> accounts = new List<Account>();
-            accounts = Account.ReadCredentialsFromFile("C:\\Users\\andra\\Documents\\Facultate\\II\\sem II\\MVP\\Teme\\RezolvariTeme\\DictionaryApp\\DictionaryApp\\Resources\\AdminsAccounts.txt");
+            accounts = Account.ReadCredentialsFromFile("C:\\Users\\andra\\Documents\\Facultate\\II\\sem II\\MVP\\Teme\\RezolvariTeme\\DictionaryApp\\DictionaryApp\\Resources\\files\\AdminsAccounts.txt");
 
            
             foreach (Account account in accounts)
@@ -80,6 +80,83 @@ namespace DictionaryApp
             this.Close();
         }
 
+        private void UploadButton_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dialog = new Microsoft.Win32.OpenFileDialog();
 
+            dialog.Filter = "Imagini (*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp|Toate fișierele (*.*)|*.*";
+
+            if (dialog.ShowDialog() == true)
+            {
+                try
+                {
+                    imagePath = dialog.FileName;
+                    string destinationFolder = @"C:\Users\andra\Documents\Facultate\II\sem II\MVP\Teme\RezolvariTeme\DictionaryApp\DictionaryApp\Resources\images";
+                    string destinationPath = System.IO.Path.Combine(destinationFolder, System.IO.Path.GetFileName(imagePath));
+                    File.Copy(imagePath, destinationPath, true);
+                    imagePath = destinationPath;
+                
+                    BitmapImage image = new BitmapImage(new Uri(imagePath));
+                    WordsImage.Source = image;
+                    WordsImage.Visibility = Visibility.Visible;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("A apărut o eroare: " + ex.Message, "Eroare", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+
+        }
+
+        private void UploadWordButton_Click(object sender, RoutedEventArgs e)
+        {
+            string word = WordTextBox.Text;
+            string description = DescriptionTextBox.Text;
+            string category = CategoryTextBox.Text;
+            string image = imagePath;
+
+            WordsManager manager = new WordsManager();
+            manager.AddWord(category,word,description,image);
+
+            WordTextBox.Text = string.Empty;
+            DescriptionTextBox.Text = string.Empty;
+            imagePath = string.Empty;
+            WordsImage.Visibility = Visibility.Hidden;
+            CategoryTextBox.Text = string.Empty;
+
+            System.Windows.MessageBox.Show("Felicitări! Cuvântul a fost adăugat.", "Mesaj de Validare", MessageBoxButton.OK);
+        }
+
+        private void CategoryTextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            WordsManager manager = new WordsManager();
+            CategoryComboBox.ItemsSource = manager.GetExistingCategories(); // Metoda pentru a obține categoriile disponibile
+            CategoryComboBox.Visibility = Visibility.Visible;
+        }
+
+        private void CategoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+            if (CategoryComboBox.SelectedItem != null)
+            {
+                if (CategoryComboBox.SelectedItem.ToString() == "Adaugă categorie")
+                {
+                    CategoryTextBox.Visibility = Visibility.Visible;
+                    CategoryTextBox.Focus(); // Adu textbox-ul in fata pentru introducerea categoriei
+                }
+                else
+                {
+                    CategoryTextBox.Visibility = Visibility.Collapsed;
+                    CategoryTextBox.Text = CategoryComboBox.SelectedItem.ToString();
+                }
+            }
+            // Ascunde ComboBox-ul după selectare
+            CategoryComboBox.Visibility = Visibility.Collapsed;
+        }
+
+        private void CategoryTextBox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+        
+        }
     }
 }
